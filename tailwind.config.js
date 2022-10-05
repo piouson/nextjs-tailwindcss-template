d92e87e810289d6a756a27c4da59a92a573a3a05
Pius Lawal <Pius.Lawal@gft.com>
2022-10-05 20:11:04 +0100

    🚨 add auto lint and format on save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,15 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'indigo',
+      },
+    },
+    screens: {
+      xs: '480px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
